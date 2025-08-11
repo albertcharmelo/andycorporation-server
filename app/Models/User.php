7686ff946a97853695cc07ea9fa76676fde264a6
Albert Charmelo <86924110+albertchg@users.noreply.github.com>
2025-08-10 21:42:13 -0400

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(CartItem::class);
     }
+
+    public function addresses()
+    {
+        return $this->hasMany(UserAddress::class);
+    }
 }
