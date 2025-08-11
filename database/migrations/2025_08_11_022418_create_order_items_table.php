869f8ca3f691fb5bca490da2b2a1b1c8d9be1143
Albert Charmelo <86924110+albertchg@users.noreply.github.com>
2025-08-11 00:54:16 -0400

    Create 2025_08_11_022418_create_order_items_table.php

---
+++ database/migrations/2025_08_11_022418_create_order_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            // $table->uuid('id')->primary(); // UUID como ID primario
+            $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->string('product_name');              // Almacena el nombre por si el producto es eliminado
+            $table->decimal('price_at_purchase', 10, 2); // Precio del producto en el momento de la compra
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
