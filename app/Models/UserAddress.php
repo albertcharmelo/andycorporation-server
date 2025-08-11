d907837cfa548dcdb221bc1d733073eca1f190fa
Albert Charmelo <86924110+albertchg@users.noreply.github.com>
2025-08-10 21:42:15 -0400

    Create UserAddress.php

---
+++ app/Models/UserAddress.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserAddress extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'address_line_1',
+        'address_line_2',
+        'postal_code',
+        'is_default',
+        'latitude',
+        'longitude',
+    ];
+
+    protected $casts = [
+        'is_default' => 'boolean',
+        'latitude' => 'decimal:8',   // Castea a decimal con 8 decimales
+        'longitude' => 'decimal:8',  // Castea a decimal con 8 decimales
+    ];
+
+    /**
+     * Una dirección pertenece a un usuario.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
